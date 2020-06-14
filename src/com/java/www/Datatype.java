b695d78e7ed90d3b896f274b88c02854c930f5d4
sanghojegal <sanghojegal@localhost>
2020-06-14 17:31:05 +0900

    second commit

---
+++ src/com/java/www/Datatype.java
@@ -0,0 +1,14 @@
+package com.java.www;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println(6);
+	System.out.println("six");
+	System.out.println("6");
+	System.out.println("6"+"6");
+	
+	}
+
+}
